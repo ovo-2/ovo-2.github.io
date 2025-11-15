299c1c108565b4a227177840c04d2bd820c4e76e
Haleyschool <lehaichqlaocai@gmail.com>
2025-11-16 02:09:13 +0700

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,17 +1,13 @@
-// Defer loading of Google Analytics
-window.addEventListener('load', function() {
-  setTimeout(function() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-K85477H479';
-    document.head.appendChild(script);
-    
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-K85477H479');
-  }, 1000);
-});
+// Google tag (gtag.js)
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-NZXTWC73GE';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-NZXTWC73GE');
 
